f0d6a458bbd9e54380172d895ecc1653959d0f70
Xuong Huynh <longxuongz@gmail.com>
2024-04-01 00:26:24 +0700

    Create course card component

---
+++ components/course-card.tsx
@@ -0,0 +1,46 @@
+import IconBadget from '@/components/icon-badge'
+import { formatPrice } from '@/lib/format'
+import { BookOpen } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+interface CourseCardProps {
+    id: string
+    title: string
+    imageUrl: string
+    category: string | undefined
+    chaptersLength: number
+    progress: number
+    price: number 
+}
+
+const CourseCard = ({id, title, imageUrl, category, chaptersLength, progress, price}: CourseCardProps) => {
+  return (
+    <Link href={`/courses/${id}`}>
+        <div className="group hover:shadow-sm transition overflow-hidden border rounded-lg p-3 h-full">
+            <div className="relative w-full aspect-video rounded-md overflow-hidden">
+                <Image src={imageUrl} alt={title} fill className="object-cover"/>
+
+            </div>
+            <div className="flex flex-col pt-2">
+                <div className="text-lg md:text-base font-medium group-hover:text-sky-700 transition line-clamp-2">{title}</div>
+            </div>
+            <p className='text-xs text-muted-foreground'>{category}</p>
+            <div className="my-3 flex items-center gap-x-2 text-sm md:text-xs">
+                <div className="flex items-center gap-x-1 text-slate-500">
+                    <IconBadget icon={BookOpen} size="sm" />
+                    <span>{chaptersLength} {chaptersLength === 1 ? 'chapter' : 'chapters'}</span>
+                </div>
+            </div>
+            {progress !== null ? (
+                <div>TODO: Course Progress</div>
+            ) : (
+                <p className='text-md md:text-sm font-medium text-slate-700'>{formatPrice(price)}</p>
+            )}
+        </div>
+    </Link>
+  )
+}
+
+export default CourseCard
--- components/courses-list.tsx
+++ components/courses-list.tsx
@@ -1,3 +1,4 @@
+import CourseCard from "@/components/course-card";
 import { Category, Course } from "@prisma/client";
 import React from "react";
 
@@ -20,11 +21,16 @@ const CoursesList = ({ items }: CoursesListProps) => {
             )}
             <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-4">
                 {items.map((item) => (
-                    <div key={item.id}>
-                        <h3>{item.title}</h3>
-                        <p>{item.description}</p>
-                        <p>{item.category?.name}</p>
-                    </div>
+                    <CourseCard
+                        key={item.id}
+                        id={item.id}
+                        title={item.title}
+                        chaptersLength={item.chapters.length}
+                        category={item?.category?.name}
+                        progress={item.progress!}
+                        price={item.price!}
+                        imageUrl={item.imageUrl!}
+                    />
                 ))}
             </div>
         </div>
